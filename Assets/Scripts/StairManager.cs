ba125b847edbfdcf33a069cb325ff1cfa018120c
Tyler Sloss <tyler.sloss@mymail.champlain.edu>
2021-02-28 15:18:45 -0500

    stair bug squashed

--- Assets/Scripts/StairManager.cs
+++ Assets/Scripts/StairManager.cs
@@ -50,12 +50,15 @@ public class StairManager : MonoBehaviour
         {
             effector2D.surfaceArc = 0f;
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
         if (col.tag.Equals("Player") && !col.isTrigger && top)
         {
             effector2D.surfaceArc = 180f;
         }
     }
-
     //these two are needed for the inputs to work
     private void OnEnable()
     {
