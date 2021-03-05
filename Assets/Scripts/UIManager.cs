02b40b58432ed140b05f14e710b2ca9d4d75037e
Tyler Sloss <tyler.sloss@mymail.champlain.edu>
2021-03-04 21:36:13 -0500

    Pause Menu, Credits and Main menu

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    private bool paused = false;
+    public GameObject pauseMenu;
+    PlayerActions inputs;
+
+    private void Awake()
+    {
+        inputs = new PlayerActions();
+        inputs.Move.Pause.performed += ctx => PauseScene();
+    }
+
     public void GoToScene(int scene)
     {
         SceneManager.LoadScene(scene);
@@ -13,5 +23,31 @@ public class UIManager : MonoBehaviour
     public void ExitScene()
     {
         Application.Quit();
+    }
+
+    public void PauseScene()
+    {
+        if (!paused)
+        {
+            paused = true;
+            pauseMenu.SetActive(true);
+        }
+        else
+        {
+            paused = false;
+            pauseMenu.SetActive(false);
+        }
+    }
+
+
+    //these two are needed for the inputs to work
+    private void OnEnable()
+    {
+        inputs.Move.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputs.Move.Disable();
     }
 }
