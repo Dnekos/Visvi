587a2a61dac678b608f08b1f2e65bb1cacd54104
Demetri <dnekos2462@gmail.com>
2021-03-04 21:28:59 -0500

    created sound manager

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SoundManager : MonoBehaviour
+{
+
+    static public AudioSource sfxSource;
+    public AudioSource musicSource;
+
+    [SerializeField]
+    AudioClip MenuMusic;
+    [SerializeField]
+    AudioClip LevelMusic;
+
+    [SerializeField]
+    static AudioClip ButtonSFX;
+    [SerializeField]
+    static AudioClip PickupSFX;
+
+    private void Awake()
+    {
+
+        sfxSource = GetComponentInChildren<AudioSource>();
+        DontDestroyOnLoad(transform.gameObject);
+        SceneManager.activeSceneChanged += ChangedActiveScene;
+
+        PickupSFX = Resources.Load<AudioClip>("Sounds/eat_sfx");
+        ButtonSFX = Resources.Load<AudioClip>("Sounds/button_sfx");
+    }
+
+    static public void PlayPickupSFX()
+    {
+        sfxSource.PlayOneShot(PickupSFX);
+    }
+
+    static public void PlayButtonSFX()
+    {
+        sfxSource.PlayOneShot(ButtonSFX);
+    }
+
+
+    private void ChangedActiveScene(Scene current, Scene next)
+    {
+
+        switch (next.buildIndex)
+        {
+            case 0: // main menu
+            case 2: // credits
+                if (!(musicSource.isPlaying && musicSource.clip == MenuMusic))
+                {
+                    musicSource.clip = MenuMusic;
+                    musicSource.Play();
+                }
+                break;
+            case 1:
+                musicSource.clip = LevelMusic;
+                musicSource.Play();
+                break;
+        };
+
+    }
+}
