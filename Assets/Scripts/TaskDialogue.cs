57aa67e548ac9e6e37796a96905dc1836624aa7a
Demetri <dnekos2462@gmail.com>
2021-03-07 18:54:33 -0500

    Cleanup UI

--- Assets/Scripts/TaskDialogue.cs
+++ Assets/Scripts/TaskDialogue.cs
@@ -30,7 +30,6 @@ public class TaskDialogue
         switch (index)
         {
             case Pickup.Basket:
-                //name = "\u13D4\u13B7\u13E3";
                 op_dialogue = new string[] { "Before we start getting ingredients,",
                     "you will need something to hold them in.",
                     "My ᏔᎷᏣ (Talutasa) should be upstairs in my room." };
@@ -38,8 +37,6 @@ public class TaskDialogue
                 end_dialogue = new string[] { "Now we can begin gathering our ingredients." };
                 break;
             case Pickup.Grapes:
-                //name = "\u13A4\u13B5\u13AB\u13AB"; // cup name
-                //name = "spoons";
                 op_dialogue = new string[] { "The first ingredient we’ll need to make the grape dumplings",
                     "is the main ingredient themself.", "Go get some ᎤᏂᏖᎸᎳᏗ (Unitelvladi) from the vines in the garden." };
                 hint_dialogue = new string[] { "You’ll have to go outside to the left to get to the garden.",
@@ -47,21 +44,18 @@ public class TaskDialogue
                 end_dialogue = new string[] { "Thank you child, these ᎤᏂᏖᎸᎳᏗ (Unitelvladi) are lovely." };
                 break;
             case Pickup.Sugar:
-                //name = "\u13A2\u13D2\u13E9\u13C2\u13A8";
                 op_dialogue = new string[] { "These dumplings are going to be quite a treat.",
                     "Go get some ᎧᎵᏎᏥ (Kalisetsi) from the Kitchen to sweeten these up." };
                 hint_dialogue = new string[] { "It’s in the cupboard on the right.", "It’s in the kitchen." };
                 end_dialogue = new string[] { "Wonderful, these will be quite a sweet treat!" };
                 break;
             case Pickup.Mixingbowl:
-                //name = "\u13A4\u13C2\u13D6\u13B8\u13B3\u13D7";
                 op_dialogue = new string[] { "We need a ᎠᏟᏍᏙᏗ (Atlisdodi) in order to mix the ingredients in.",
                     "There should be one in the Kitchen." };
-                hint_dialogue = new string[] { "It's wooden and brown.", "I think it's on the counter." };
+                hint_dialogue = new string[] { "It's wooden and brown.", "I think it's on the counter in the Kitchen." };
                 end_dialogue = new string[] { "Now let’s get the last ingredients." };
                 break;
             case Pickup.Flour:
-                name = "";
                 op_dialogue = new string[] { "We’ll need something to hold all of our ingredients together!",
                     "Can you get me the bag of ᎢᏒᏩᏂᎨ (Isvwanige)." };
                 hint_dialogue = new string[] { "It’s in the cupboard in a [COLOR] bag.",
@@ -69,7 +63,6 @@ public class TaskDialogue
                 end_dialogue = new string[] { "Lovely,","we’re close to having all of the ingredients for our grape dumplings." };
                 break;
             case Pickup.Mint:
-                name = "";
                 op_dialogue = new string[] { "Hm, why don’t we add a little bit of something to make it look pretty?",
                     "Could you gather ᎠᏕᎸᏧᏃᏢᏗ from from the garden?" };
                 hint_dialogue = new string[] { "It has small pointed green leaves.",
@@ -77,7 +70,6 @@ public class TaskDialogue
                 end_dialogue = new string[] { "Thank you, this will make for a lovely decoration on our dish." };
                 break;
             case Pickup.Juice:
-                name = "";
                 op_dialogue = new string[] { "The meal will be cooked in some ᎦᏁᎲ (Ganehv)." };
                 hint_dialogue = new string[] { "ᎦᏁᎲ is made by juicing grapes.",
                     "You should be able to find a jar in the fridge.",
@@ -85,26 +77,22 @@ public class TaskDialogue
                 end_dialogue = new string[] { "Thank you, now we should have all the ingredients to make the grape dumplings." };
                 break;
             case Pickup.Spoon:
-                name = "";
                 op_dialogue = new string[] { "We need something to stir this with.", "Could you go get a  ᎠᏗᏙᏗ (Adidodi)?" };
                 hint_dialogue = new string[] { "I keep them with the silverware.", "They are kept in the kitchen." };
                 end_dialogue = new string[] { "Now with a ᎠᏗᏙᏗ (Adidodi) I can finish making the grape dumplings." };
                 break;
             case Pickup.ServingBowl:
-                name = "";
                 op_dialogue = new string[] { "Lastly we’ll need something special to put our grape dumplings in.",
                     "Could you get me two ᎠᏟᏍᏙᏗ (Atlisdodi)" };
                 hint_dialogue = new string[] { "They’re in the living room.", "They’re in a special cabinet in the living room." };
                 end_dialogue = new string[] { "Thank you dear, these were a special gift from one of your ancestors." };
                 break;
             case Pickup.Napkin:
-                name = "";
                 op_dialogue = new string[] { "Can you help set the table?", "We'll need some [NAPKINS]." };
                 hint_dialogue = new string[] { "They are folded up by the side of the table.", "They should be in the dining room." };
                 end_dialogue = new string[] { "Thank you, I think we are ready." };
                 break;
             default:
-                name = "";
                 op_dialogue = new string[] { "Can you give me my " + name + "?" };
                 hint_dialogue = new string[] { "Its around here somewhere, keep looking." };
                 end_dialogue = new string[] { "Thank You" };
